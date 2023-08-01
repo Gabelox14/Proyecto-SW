ab9f4a06794e7b0dbe2bb446bfd7d35059e94825
Angel Gabriel Ureña Quesada <61789851+Gabelox14@users.noreply.github.com>
2023-07-31 21:23:52 -0600

    beta App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { RiPizzaLine } from 'react-icons/ri';
+import { FaIceCream } from 'react-icons/fa';
+
+const foodItems = [
+  {
+    name: 'Hamburger',
+    image: 'https://im4g3s.blob.core.windows.net/img-ws/Hamburger_(black_bg).jpg',
+    price: 8.99,
+    category: 'Burgers',
+    icon: <GiHamburgerMenu />,
+  },
+  {
+    name: 'Pizza',
+    image: 'https://im4g3s.blob.core.windows.net/img-ws/classic-cheese-pizza-recipe-2-64429a0cb408b.jpg',
+    price: 12.99,
+    category: 'Pizzas',
+    icon: <RiPizzaLine />,
+  },
+  {
+    name: 'Ice Cream',
+    image: 'https://im4g3s.blob.core.windows.net/img-ws/helado-cono-1-1.jpg',
+    price: 4.99,
+    category: 'Desserts',
+    icon: <FaIceCream />,
+  },
+];
 
 function App() {
-  const names = ['Montero', 'Angel', 'Ari', 'Gabo', 'Amy'];
-  const [currentNameIndex, setCurrentNameIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentNameIndex((prevIndex) => (prevIndex + 1) % names.length);
-    }, 4000);
-
-    return () => clearInterval(interval);
-  }, [names.length]);
-
-  const currentName = names[currentNameIndex];
-
-  return <div>Hello {currentName}</div>;
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        {foodItems.map((food, index) => (
+          <div key={index} className="col-md-4 mb-3">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={food.image} />
+              <Card.Body>
+                <Card.Title>{food.name}</Card.Title>
+                <Card.Text>Price: ${food.price.toFixed(2)}</Card.Text>
+                <Card.Text>Category: {food.category}</Card.Text>
+                <Button variant="primary">{food.icon} Add to Cart</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
