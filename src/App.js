1541264164b82dd73bb1b3e2bdfdbd4a3bdfec22
Angel Gabriel Ureña Quesada <61789851+Gabelox14@users.noreply.github.com>
2023-07-31 21:01:56 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const names = ['Montero', 'Angel', 'Ari', 'Gabo', 'Amy'];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentName, setCurrentName] = useState(names[currentIndex]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % names.length);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>Hello {currentName}</div>;
 }
 
 export default App;
