0f4587ed6997b0eec86154df60a7f4c2b8d00692
gabrielnunez99 <gnunezp483@ulacit.ed.cr>
2023-08-18 23:31:27 -0600

    UPDATE 12
    UPDATE 12

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,67 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
 import { useNavigate } from 'react-router-dom';
 import login from '../img/login.png';
-import { useMsal } from '@azure/msal-react';
+
+const supabase = createClient(
+  'https://amlztwycpfhonwfvzhca.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtbHp0d3ljcGZob253ZnZ6aGNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ0ODI1NTksImV4cCI6MTk5MDA1ODU1OX0.NqV1vbExN3jpY11rTWPN4fEvEw-m5xjNmHVh2GwGIsI'
+);
 
 function Login() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [address, setAddress] = useState('');
-  const [phone, setPhone] = useState('');
 
-  const { instance } = useMsal();
-  const history = useNavigate();
-
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    const data = {
-      name: "name",
-      address: "address",
-      phone: "phone",
-      email: "email@gmail.com",
-    };
-
-    try {
-      const response = await fetch('/data-api/rest/dbservicios/Usuario', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        console.log('Data successfully added to the database.');
-      } else {
-        console.error('Failed to add data to the database.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
+  const navigate = useNavigate();
+  supabase.auth.onAuthStateChange(async (event) => {
+    if (event === 'SIGNED_IN') {
+      navigate('/home');
+    } else {
+      navigate('/');
     }
-  }
-
-  const handleLogin = async () => {
-    const loginRequest = {
-      scopes: ['openid', 'profile', 'email'],
-    };
-
-    await instance.loginRedirect(loginRequest);
-  };
-
-  useEffect(() => {
-    const checkAuthenticationStatus = async () => {
-      const accounts = await instance.getAllAccounts();
-
-      if (accounts.length > 0) {
-        history('/home');
-      } else {
-        history('/');
-      }
-    };
-
-    checkAuthenticationStatus();
-  }, [instance, history]);
+  });
 
   async function list() {
     const endpoint = '/data-api/rest/dbservicios';
@@ -70,122 +26,95 @@ function Login() {
     console.table(data.value);
   }
 
+
   async function get() {
     const id = 1;
-    const endpoint = `/data-api/rest/dbservicios/${id}`; // Corrected endpoint
-    const response = await fetch(endpoint);
+    const endpoint = `/data-api/rest/dbservicios/Id`;
+    const response = await fetch(`${endpoint}/${id}`);
+    const result = await response.json();
+    console.table(result.value);
+  }
+  
+
+  async function update() {
+
+    const id = 1;
+    const data = {
+      Name: "Molly"
+    };
+  
+    const endpoint = '/data-api/rest/dbservicios/Id';
+    const response = await fetch(`${endpoint}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
     const result = await response.json();
     console.table(result.value);
   }
 
-  async function update() {
-    const data = {
-      Name: 'Molly',
-    };
 
-    const endpoint = '/data-api/rest/dbservicios/1'; // Corrected endpoint
+  async function create() {
+
+    const data = {
+      name: "Pedro",
+      email: "ag@gmail.com",
+      address: "casa",
+      phont: "123456"
+    };
+  console.log(data);
+    const endpoint = `/data-api/rest/dbservicios/`;
     const response = await fetch(endpoint, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
     });
     const result = await response.json();
     console.table(result.value);
+
+    if (result === 200) {
+      navigate('/home');
+    } else {
+      navigate('/');
+    }
   }
 
   async function del() {
     const id = 3;
-    const endpoint = `/data-api/rest/dbservicios/${id}`; // Corrected endpoint
-    const response = await fetch(endpoint, {
-      method: 'DELETE',
+    const endpoint = '/data-api/rest/dbservicios/Id';
+    const response = await fetch(`${endpoint}/${id}`, {
+      method: "DELETE"
     });
-    if (response.ok) {
-      console.log(`Record deleted: ${id}`);
+    if(response.ok) {
+      console.log(`Record deleted: ${ id }`)
     } else {
       console.log(response);
     }
   }
 
+
   return (
     <>
-      <div>
-        <button id="list" onClick={list}>
-          List
-        </button>
-        <button id="get" onClick={get}>
-          Get
-        </button>
-        <button id="update" onClick={update}>
-          Update
-        </button>
-        <button id="delete" onClick={del}>
-          Delete
-        </button>
-      </div>
+<div>
+</div>
+
       <div className="container mx-auto flex justify-center py-8 text-white">
         <div className="flex-1 h-full max-w-full md:ml-20 md:mr-16 mt-4 bg-bgColor">
           <div className="flex flex-col md:flex-row">
             <div className="h-32 md:h-auto md:w-1/2">
-              <img className="object-cover w-full h-full" src={login} alt="login" />
+              <img className="object-cover w-full h-full" src={login}
+                alt="login" />
             </div>
             <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
               <div className="w-72">
                 <h1 className="mb-4 text-2xl font-bold text-center text-white">
                   Signup/Login to Your Account
                 </h1>
-                <div>
-                  <h1>User Information</h1>
-                  <form onSubmit={handleSubmit}>
-                    <label htmlFor="name">Name:</label>
-                    <input
-                      type="text"
-                      id="name"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                      required
-                    />
-                    <br />
-                    <br />
-
-                    <label htmlFor="address">Address:</label>
-                    <input
-                      type="text"
-                      id="address"
-                      value={address}
-                      onChange={(e) => setAddress(e.target.value)}
-                      required
-                    />
-                    <br />
-                    <br />
-
-                    <label htmlFor="phone">Phone:</label>
-                    <input
-                      type="text"
-                      id="phone"
-                      value={phone}
-                      onChange={(e) => setPhone(e.target.value)}
-                      required
-                    />
-                    <br />
-                    <br />
-
-                    <label htmlFor="email">Email:</label>
-                    <input
-                      type="email"
-                      id="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      required
-                    />
-                    <br />
-                    <br />
-
-                    <button type="submit">Submit</button>
-                  </form>
-                </div>
-                <div>
-                  <button onClick={handleLogin}>Login with Google</button>
-                </div>
+                <button type="submit" onClick={create}>Create</button>
+                <button type="submit" onClick={list}>List</button>
+                <button type="submit" onClick={get}>Get</button>
+                <button type="submit" onClick={update}>Update</button>
+                <button type="submit" onClick={del}>Delete</button>
               </div>
             </div>
           </div>
