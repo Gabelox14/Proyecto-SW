4a71c9b9974900e6ab6e49e05e51af75f7cbf31e
gabrielnunez99 <gnunezp483@ulacit.ed.cr>
2023-08-22 02:10:13 -0600

    grgsfdgf
    fgfd

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -65,8 +65,8 @@ function Login() {
 
     const data = {
       name: name,
-      email: email
-      //password_hash: password
+      email: email,
+      phone: password
     };
   
     const endpoint = `/data-api/rest/dbservicios/`;
