923fafca8e4317f5b47f1ea45da064f27d1007e2
lmontero18 <lmonterom879@gmail.com>
2023-08-24 00:27:52 -0600

    Dish#5

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -1,20 +1,29 @@
 import "../styles/button.css";
+import React, { useState } from "react";
+
 // import React, { useState } from "react";
 
 
-interface Item {
-    title: string;
-    price: number;
-    img: string;
-}
+// interface Item {
+//     title: string;
+//     price: number;
+//     img: string;
+// }
 
 
 
 
-const Cards = ({ item, handleClick }: { item: Item, handleClick: (item: Item) => void }) => {
-    const { title, price, img } = item;
-    
-    
+const Cards = () => {
+    // const { title, price, img } = item;
+    // //const [dishTittle] = useState("");
+    // const [dishPrice] = useState("");
+    // const [dishImg] = useState("");
+    // const [dishKind] = useState("");
+    // const [dishAmount] = useState("");
+    const [dishData, setDishData] = useState([]);
+
+
+
     async function get() {
         try{
             const endpointDish = '/data-api/rest/dishservicios/';
@@ -23,15 +32,27 @@ const Cards = ({ item, handleClick }: { item: Item, handleClick: (item: Item) =>
 
             console.table(result.value);
 
+            // for (let i = 0; i < result.value.length; i++) {
+            //     const dish = result.value[i];
+            //      //dishID = dish.dish_id;
+            //      //setPrice = dish.price;
+            //      const dishTittle = dish.tittle;
+            //      //dishImg = dish.imgURL;
+            //      //dishKind = dish.kind;
+            //      //dishAmount = dish.amount;
+
+            // }
+            setDishData(result.value);
+
         } catch{
-        
+            console.log("error");
         }
     }
 
-    
-    
 
-   
+
+
+
 
     return (
         <>
@@ -40,10 +61,10 @@ const Cards = ({ item, handleClick }: { item: Item, handleClick: (item: Item) =>
                     <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-auto">
                         <img className="lg:h-72 md:h-36 h-48 w-full object-cover object-center" src={img} alt="item" />
                         <div className="px-3 py-2">
-                            <h1 className="text-xl font-bold mb-3">{title}</h1>
+                            <h1 className="text-xl font-bold mb-3" >{dish.tittle}</h1>
                             <div className="flex flex-wrap justify-between mb-2">
                                 <p className="leading-relaxed mt-4 text-lg">Price:  ₡{price}</p>
-                                <button onClick={() => handleClick(item)} className="animated-btn">Add to Cart</button>
+                                {/* <button onClick={() => handleClick(item)} className="animated-btn">Add to Cart</button> */}
                                 <button onClick={(get)}>Add to Cart</button>
                             </div>
                         </div>
--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -1,7 +1,7 @@
 //import React, { useState, useEffect } from 'react';
 import React, { useState } from "react";
 import Cards from './Cards';
-import { list } from '../data';
+// import { list } from '../data';
 import { FaSearch } from "react-icons/fa";
 
 //interface CartItem {
